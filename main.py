c9b52c1939dc0f3ad8c7df6d4f0c3c5eaeb2ae7e
hyper750 <raulmarquespalmer_150@hotmail.com>
2021-10-13 18:47:55 +0200

    Initialize DB and MA

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from flask import Flask
 
-from settings import FLASK_CONFIG
+from settings import FLASK_CONFIG, DB, MA
 
 app = Flask('Commander')
 
 # Configure app
 app.config.update(FLASK_CONFIG)
+
+# Initialize database
+DB.init_app(app)
+
+# Initialize db serializer
+MA.init_app(app)
+
+# TODO: Initialize jwt
+
+# TODO: Initialize api resources
 
 if __name__ == '__main__':
     app.run()
--- settings.py
+++ settings.py
@@ -1,11 +1,19 @@
 import os
 
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+
+HASH_NAME = 'sha512'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+ITERATIONS = 5000
+
+
 FLASK_CONFIG = {
     # Production mode
     'DEBUG': os.environ.get('DEBUG') in ['True', '1', 't'],
 
     # Secret key
-    'SECRET_KEY': os.environ.get('SECRET_KEY'),
+    'SECRET_KEY': SECRET_KEY,
 
     # Database url
     # Eg: postgresql://root:password@127.0.0.1:5432/dbname
@@ -14,3 +22,7 @@ FLASK_CONFIG = {
     # Disable before and after commit signals
     'SQLALCHEMY_TRACK_MODIFICATIONS': False,
 }
+
+
+DB = SQLAlchemy()
+MA = Marshmallow()
