658fca2c89b25ab6ef2a785425cd08395ef33fea
hyper750 <raulmarquespalmer_150@hotmail.com>
2021-10-16 22:21:55 +0200

    Added auth login response

--- resources/login.py
+++ resources/login.py
@@ -1,13 +1,22 @@
 from flask_jwt_extended import create_access_token
 from flask_restful import Resource
-from logic.user import parse_login
+from logic.user import authenticate_user, parse_login
 
 
 class Auth(Resource):
 
     def post(self):
         parameters = parse_login()
-        return 'create jwt'
+
+        if user := authenticate_user(**parameters):
+            token = create_access_token(user.id)
+            return {
+                'token': token
+            }
+
+        return {
+            'error': 'User or password are not correct'
+        }, 401
 
     def delete(self):
         return 'destroy jwt'
