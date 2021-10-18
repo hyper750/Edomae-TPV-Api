e47b1a89e8ff74a043a5f6d0c0275216dfedf1e1
hyper750 <raulmarquespalmer_150@hotmail.com>
2021-10-18 18:12:09 +0200

    Added expired and revoked response

--- logic/user/authentication.py
+++ logic/user/authentication.py
@@ -38,3 +38,17 @@ def check_if_token_is_revoked(jwt_header, jwt_payload):
     redis = create_redis_revoke_jwt_token()
     token_in_redis = redis.get(jti)
     return token_in_redis is not None
+
+
+@JWT.expired_token_loader
+def custom_expired_token_response(jwt_header, jwt_payload):
+    return {
+        'msg': 'Access token expired'
+    }
+
+
+@JWT.revoked_token_loader
+def custom_revoked_token_response(jwt_header, jwt_payload):
+    return {
+        'msg': 'Access token revoked'
+    }
