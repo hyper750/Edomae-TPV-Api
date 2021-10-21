4067a8e1185e455aff3bd975143a162dafd3d3fd
hyper750 <raulmarquespalmer_150@hotmail.com>
2021-10-21 20:15:58 +0200

    Added meal category parsers

---
+++ logic/meal_category/__init__.py
@@ -0,0 +1,4 @@
+from .meal_category_parser import (
+    parse_object_meal_category,
+    parse_query_meal_category
+)
---
+++ logic/meal_category/meal_category_parser.py
@@ -0,0 +1,18 @@
+from flask_restful import reqparse
+
+
+def parse_object_meal_category() -> dict:
+    parser = reqparse.RequestParser()
+    parser = parser.add_argument('name', required=True, type=str)
+    parser = parser.add_argument('imatge_name', required=True, type=str)
+
+    return parser.parse_args()
+
+
+def parse_query_meal_category() -> dict:
+    parser = reqparse.RequestParser()
+
+    parser = parser.add_argument('name', type=str, store_missing=False)
+    parser = parser.add_argument('imatge_name', type=str, store_missing=False)
+
+    return parser.parse_args()
