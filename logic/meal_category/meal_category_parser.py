f7e0a792498b95c72199c625ab69fabe3268203e
hyper750 <raulmarquespalmer_150@hotmail.com>
2021-10-21 20:16:36 +0200

    Added blank space :))

--- logic/meal_category/meal_category_parser.py
+++ logic/meal_category/meal_category_parser.py
@@ -3,6 +3,7 @@ from flask_restful import reqparse
 
 def parse_object_meal_category() -> dict:
     parser = reqparse.RequestParser()
+    
     parser = parser.add_argument('name', required=True, type=str)
     parser = parser.add_argument('imatge_name', required=True, type=str)
 
