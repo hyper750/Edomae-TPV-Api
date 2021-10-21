f14d9f7340ec69cb2bd9321ac237b461b908f7cc
hyper750 <raulmarquespalmer_150@hotmail.com>
2021-10-21 20:11:18 +0200

    Added meal category query, post and put

--- resources/meal_category.py
+++ resources/meal_category.py
@@ -1,6 +1,12 @@
+from adapter.db import DB
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource
+from logic.meal_category import (
+    parse_object_meal_category,
+    parse_query_meal_category
+)
 from models import MealCategory, MealSchema
+from models.meal_category import MealCategorySchema
 
 
 class MealCategoryResource(Resource):
@@ -19,14 +25,35 @@ class MealCategoryResource(Resource):
         return '', 204
 
     def put(self, id: int):
-        pass
+        category = MealCategory.query.get(id)
+
+        if not category:
+            return '', 404
+
+        params = parse_query_meal_category()
+        for key, value in params.items():
+            setattr(category, key, value)
+
+        DB.session.commit()
+
+        return MealCategorySchema().dump(category)
 
 
 class MealCategoriesResource(Resource):
     method_decorators = (jwt_required(),)
 
     def post(self):
-        pass
+        category = MealCategory(
+            **parse_object_meal_category()
+        )
+
+        DB.session.add(category)
+        DB.session.commit()
+
+        return MealCategorySchema().dump(category), 201
 
     def get(self):
-        pass
+        categories = MealCategory.query.filter_by(
+            **parse_query_meal_category()
+        )
+        return MealCategorySchema(many=True).dump(categories)
