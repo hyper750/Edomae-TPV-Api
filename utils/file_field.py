56998a7279e8979e99bbdefe147b2de2456cf080
hyper750 <raulmarquespalmer_150@hotmail.com>
2021-10-23 17:23:01 +0200

    Create folder if not exists

--- utils/file_field.py
+++ utils/file_field.py
@@ -4,6 +4,9 @@ from werkzeug.datastructures import FileStorage
 
 
 def save_file_field(file_field: FileStorage, dir: str):
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+
     file_field.save(
         os.path.join(dir, file_field.filename)
     )
