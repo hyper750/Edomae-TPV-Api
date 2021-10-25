7359b44582b8ec7c9328f396f4e48f32f341e9d9
hyper750 <raulmarquespalmer_150@hotmail.com>
2021-10-25 22:35:22 +0200

    Added payment method model

--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,5 @@ from .command import Command, CommandSchema
 from .command_meal import CommandMeal, CommandMealSchema
 from .meal import Meal, MealSchema
 from .meal_category import MealCategory, MealCategorySchema
+from .payment_method import PaymentMethod, PaymentMethodSchema
 from .user import User, UserSchema
---
+++ models/payment_method.py
@@ -0,0 +1,30 @@
+from adapter.db import DB, MA
+from sqlalchemy import BigInteger, Boolean, Column, String
+from sqlalchemy.sql import expression
+
+from .crud_model import CRUDModel
+
+
+class PaymentMethod(DB.Model, CRUDModel):
+    __tablename__ = 'payment_method'
+
+    id = Column(BigInteger, primary_key=True)
+
+    # Payment method enabled or not
+    enabled = Column(Boolean, nullable=False, default=expression.true())
+
+    # Payment method name, Eg: 'Credit card', 'Efectivo'
+    name = Column(
+        String(length=124),
+        nullable=False,
+        unique=True
+    )
+
+
+class PaymentMethodSchema(MA.Schema):
+    class Meta:
+        fields = (
+            'id',
+            'enabled',
+            'name'
+        )
