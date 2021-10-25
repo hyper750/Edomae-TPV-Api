431e2b65bad316cfb76ae9bc9a561de05a886835
hyper750 <raulmarquespalmer_150@hotmail.com>
2021-10-25 22:26:43 +0200

    Added model command_meal

---
+++ models/command_meal.py
@@ -0,0 +1,38 @@
+from adapter.db import DB, MA
+from sqlalchemy import BigInteger, Column, ForeignKey, Integer
+
+
+class CommandMeal(DB.Model):
+    __tablename__ = 'command_meal'
+
+    id = Column(BigInteger, primary_key=True)
+
+    # Command reference
+    command = Column(
+        BigInteger,
+        ForeignKey('command.id', ondelete='CASCADE'),
+        nullable=False
+    )
+
+    # Meal reference
+    meal = Column(
+        BigInteger,
+        ForeignKey('meal.id', ondelete='CASCADE'),
+        nullable=False
+    )
+
+    # Number of meals to get
+    number = Column(
+        Integer,
+        nullable=False
+    )
+
+
+class CommandMealSchema(MA.Schema):
+    class Meta:
+        fields = (
+            'id',
+            'command',
+            'meal',
+            'number'
+        )
