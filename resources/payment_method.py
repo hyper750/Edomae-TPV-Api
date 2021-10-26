d99bf0b2d7115c8c52e807fb694fafadc2e6aa50
hyper750 <raulmarquespalmer_150@hotmail.com>
2021-10-26 17:05:30 +0200

    Added get and delete methods

--- resources/payment_method.py
+++ resources/payment_method.py
@@ -1,18 +1,27 @@
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource
+from models import PaymentMethod, PaymentMethodSchema
 
 
 class PaymentMethodResource(Resource):
     method_decorators = (jwt_required(),)
 
     def get(self, id: int):
-        pass
+        payment_method = PaymentMethod.query.get(id)
+
+        if not payment_method:
+            return '', 404
+
+        return PaymentMethodSchema().dump(payment_method)
 
     def put(self, id: int):
         pass
 
     def delete(self, id: int):
-        pass
+        payment_method = PaymentMethod.query.get(id)
+        if payment_method:
+            payment_method.delete()
+        return '', 204
 
 
 class PaymentMethodsResource(Resource):
