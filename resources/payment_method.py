f4f4b687487afde875341687a7bba6f2def228b9
hyper750 <raulmarquespalmer_150@hotmail.com>
2021-10-26 17:27:53 +0200

    Be able to create, update and query payment methods

--- resources/payment_method.py
+++ resources/payment_method.py
@@ -1,6 +1,10 @@
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource
 from models import PaymentMethod, PaymentMethodSchema
+from logic.payment_method import (
+    parse_object_payment_method,
+    parse_query_payment_method,
+)
 
 
 class PaymentMethodResource(Resource):
@@ -15,7 +19,17 @@ class PaymentMethodResource(Resource):
         return PaymentMethodSchema().dump(payment_method)
 
     def put(self, id: int):
-        pass
+        payment_method = PaymentMethod.query.get(id)
+
+        if not payment_method:
+            return '', 404
+
+        for key, value in parse_query_payment_method().items():
+            setattr(payment_method, key, value)
+
+        payment_method.save()
+
+        return PaymentMethodSchema().dump(payment_method)
 
     def delete(self, id: int):
         payment_method = PaymentMethod.query.get(id)
@@ -28,7 +42,14 @@ class PaymentMethodsResource(Resource):
     method_decorators = (jwt_required(),)
 
     def get(self):
-        pass
+        payment_methods = PaymentMethod.query.filter_by(
+            **parse_query_payment_method()
+        )
+        return PaymentMethodSchema(many=True).dump(payment_methods)
 
     def post(self):
-        pass
+        payment_method = PaymentMethod(
+            **parse_object_payment_method()
+        )
+        payment_method.save()
+        return PaymentMethodSchema().dump(payment_method)
