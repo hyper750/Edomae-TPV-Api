bd23098208991bf8651bd8045048f42abbb4d63f
hyper750 <raulmarquespalmer_150@hotmail.com>
2021-10-26 17:55:57 +0200

    Added command parser

---
+++ logic/command/__init__.py
@@ -0,0 +1,4 @@
+from .command_parser import (
+    parse_object_command,
+    parse_query_command
+)
---
+++ logic/command/command_parser.py
@@ -0,0 +1,19 @@
+from flask_restful import reqparse
+
+
+def parse_object_command() -> dict:
+    parser = reqparse.RequestParser()
+
+    parser = parser.add_argument('paid', type=bool)
+    parser = parser.add_argument('payment_method', type=int)
+
+    return parser.parse_args()
+
+
+def parse_query_command() -> dict:
+    parser = reqparse.RequestParser()
+
+    parser = parser.add_argument('paid', type=bool, store_missing=False)
+    parser = parser.add_argument('payment_method', type=int, store_missing=False)
+
+    return parser.parse_args()
