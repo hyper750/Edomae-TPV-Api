13cddbd5002cbfab64e93ed6b132e7e2423c2f96
hyper750 <raulmarquespalmer_150@hotmail.com>
2021-10-27 22:01:08 +0200

    Added date type

--- logic/command/command_parser.py
+++ logic/command/command_parser.py
@@ -1,4 +1,4 @@
-from flask_restful import reqparse
+from flask_restful import reqparse, inputs
 
 
 def parse_object_command() -> dict:
@@ -18,8 +18,8 @@ def parse_query_command() -> dict:
     parser = parser.add_argument('page_size', type=int, default=50)
     parser = parser.add_argument('page_num', type=int, default=1)
 
-    parser = parser.add_argument('creation_date__gte', type=str, store_missing=False)
-    parser = parser.add_argument('creation_date__lte', type=str, store_missing=False)
+    parser = parser.add_argument('creation_date__gte', type=inputs.datetime_from_iso8601, store_missing=False)
+    parser = parser.add_argument('creation_date__lte', type=inputs.datetime_from_iso8601, store_missing=False)
     parser = parser.add_argument('user', type=int, store_missing=False)
     parser = parser.add_argument('paid', type=bool, store_missing=False)
     parser = parser.add_argument('payment_method', type=int, store_missing=False)
