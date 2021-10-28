57ed81a8d29ec2ea2ee75f509f9aaed66367d518
hyper750 <raulmarquespalmer_150@hotmail.com>
2021-10-28 18:33:43 +0200

    Added todo

--- utils/file_field.py
+++ utils/file_field.py
@@ -7,6 +7,8 @@ def save_file_field(file_field: FileStorage, dir: str):
     if not os.path.exists(dir):
         os.mkdir(dir)
 
+    # TODO: Create md5 hash from filename and timestamp for example, to avoid duplicates
+    # Rename the file and return the new filename
     file_field.save(
         os.path.join(dir, file_field.filename)
     )
