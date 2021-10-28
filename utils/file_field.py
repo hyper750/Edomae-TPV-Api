7e2ade52156b3ec0a2a546421a5b7a31c546bcd3
hyper750 <raulmarquespalmer_150@hotmail.com>
2021-10-28 18:46:39 +0200

    Create hash from filename

--- utils/file_field.py
+++ utils/file_field.py
@@ -1,14 +1,33 @@
+import hashlib
 import os
 
+import arrow
+from settings import HASH_NAME, ITERATIONS, SECRET_KEY
 from werkzeug.datastructures import FileStorage
 
 
-def save_file_field(file_field: FileStorage, dir: str):
+def save_file_field(file_field: FileStorage, dir: str) -> str:
     if not os.path.exists(dir):
         os.mkdir(dir)
 
     # TODO: Create md5 hash from filename and timestamp for example, to avoid duplicates
-    # Rename the file and return the new filename
+    # Split filename and fileformat
+    fileformat = file_field.filename.rsplit('.', maxsplit=1)[-1]
+
+    ts = arrow.utcnow().int_timestamp
+    new_filename = f'{file_field.filename}##{ts}'
+
+    new_filename = hashlib.pbkdf2_hmac(
+        HASH_NAME,
+        new_filename.encode('utf-8'),
+        SECRET_KEY.encode('utf-8'),
+        ITERATIONS
+    ).hex()
+
+    # Append format at the end
+    new_filename = f'{new_filename}.{fileformat}'
     file_field.save(
-        os.path.join(dir, file_field.filename)
+        os.path.join(dir, new_filename)
     )
+
+    return new_filename
