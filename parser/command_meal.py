0523377f3891be53b428a936cd0dcdbb0781849a
hyper750 <raulmarquespalmer_150@hotmail.com>
2021-11-01 22:43:08 +0100

    Be able to query meal per command

--- parser/command_meal.py
+++ parser/command_meal.py
@@ -16,6 +16,8 @@ def parse_object_command_meal() -> dict:
 def parse_query_command_meal() -> dict:
     parser = reqparse.RequestParser()
 
+    parser = parser.add_argument('command', type=int, store_missing=False)
+    parser = parser.add_argument('meal', type=int, store_missing=False)
     parser = parser.add_argument('number', type=int, store_missing=False)
     parser = parser.add_argument('extra', type=str, store_missing=False)
     parser = parser.add_argument('discount', type=float, store_missing=False)
