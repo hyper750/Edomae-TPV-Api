2f5a99e9b0df039c070bf74b4fcbb0ac1ebbf755
hyper750 <raulmarquespalmer_150@hotmail.com>
2021-11-01 16:26:13 +0100

    Added table resource

--- resources/table.py
+++ resources/table.py
@@ -1,3 +1,5 @@
+from parser import parse_object_table, parse_query_table
+
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource
 from models import Table
@@ -12,18 +14,28 @@ class TableResource(Resource):
 
         if not table:
             return '', 404
-        
+
         return TableSchema().dump(table)
 
     def put(self, id: int):
-        pass
+        table = Table.query.get(id)
+
+        if not table:
+            return '', 404
+
+        for key, value in parse_query_table().items():
+            setattr(table, key, value)
+
+        table.save()
+
+        return TableSchema().dump(table)
 
     def delete(self, id: int):
         table = Table.query.get(id)
 
         if table:
             table.delete()
-        
+
         return '', 204
 
 
@@ -31,7 +43,17 @@ class TablesResources(Resource):
     method_decorators = (jwt_required(),)
 
     def post(self):
-        pass
+        table = Table(
+            **parse_object_table()
+        )
+
+        table.save()
+
+        return TableSchema().dump(table)
 
     def get(self):
-        pass
+        tables = Table.query.filter_by(
+            **parse_query_table()
+        )
+
+        return TableSchema(many=True).dump(tables)
