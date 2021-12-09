390e1233fb37b6e069b18fab23b2a586ade1616a
hyper750 <raulmarquespalmer_150@hotmail.com>
2021-12-09 20:00:14 +0100

    Fix type boolean

--- parser/table.py
+++ parser/table.py
@@ -1,10 +1,10 @@
-from flask_restful import reqparse
+from flask_restful import inputs, reqparse
 
 
 def parse_object_table() -> dict:
     parser = reqparse.RequestParser()
 
-    parser = parser.add_argument('enabled', type=bool)
+    parser = parser.add_argument('enabled', type=inputs.boolean)
     parser = parser.add_argument('number', type=int, required=True)
     parser = parser.add_argument('local', type=int, required=True)
     parser = parser.add_argument('number_of_persons', type=int, required=True)
@@ -17,11 +17,19 @@ def parse_object_table() -> dict:
 def parse_query_table() -> dict:
     parser = reqparse.RequestParser()
 
-    parser = parser.add_argument('enabled', type=bool, store_missing=False)
+    parser = parser.add_argument(
+        'enabled', type=inputs.boolean, store_missing=False
+    )
     parser = parser.add_argument('number', type=int, store_missing=False)
     parser = parser.add_argument('local', type=int, store_missing=False)
-    parser = parser.add_argument('number_of_persons', type=int, store_missing=False)
-    parser = parser.add_argument('x_coordinates', type=float, store_missing=False)
-    parser = parser.add_argument('y_coordinates', type=float, store_missing=False)
+    parser = parser.add_argument(
+        'number_of_persons', type=int, store_missing=False
+    )
+    parser = parser.add_argument(
+        'x_coordinates', type=float, store_missing=False
+    )
+    parser = parser.add_argument(
+        'y_coordinates', type=float, store_missing=False
+    )
 
     return parser.parse_args()
