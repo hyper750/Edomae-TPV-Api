aebbcfb6d66d14a88915275fca1c5c918afa1947
hyper750 <raulmarquespalmer_150@hotmail.com>
2022-01-03 18:11:58 +0100

    Added missing slash for printer api laso

--- routes/printer_api.py
+++ routes/printer_api.py
@@ -1,7 +1,7 @@
 from flask_restful import Api
 from resources.print import PrintCommandResource
 
-PRINTER_API = Api(prefix='print')
+PRINTER_API = Api(prefix='/print')
 
 # Print command
 PRINTER_API.add_resource(PrintCommandResource, '/command/<int:id>')
