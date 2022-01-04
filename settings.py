466f97d3f91c980be2628eb26c66dde6541ced98
hyper750 <raulmarquespalmer_150@hotmail.com>
2022-01-04 17:12:21 +0100

    Added default timezone

--- settings.py
+++ settings.py
@@ -46,6 +46,9 @@ REDIS_HOST = os.environ.get('REDIS_HOST', '127.0.0.1')
 REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
 REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
 
+# Timezone
+TIMEZONE = 'Europe/Madrid'
+
 # Iva
 IVA = 21
 
