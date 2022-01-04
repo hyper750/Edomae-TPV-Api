f31087ab9de4fc875c210c45580e263bdc9cd3ad
hyper750 <raulmarquespalmer_150@hotmail.com>
2022-01-04 16:36:19 +0100

    Added iva to settings

--- settings.py
+++ settings.py
@@ -46,6 +46,9 @@ REDIS_HOST = os.environ.get('REDIS_HOST', '127.0.0.1')
 REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
 REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
 
+# Iva
+IVA = 21
+
 # Static files
 PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 
