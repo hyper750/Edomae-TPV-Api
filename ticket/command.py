f93b9eaaf685269cfdf964cd5bff66a466be3a75
hyper750 <raulmarquespalmer_150@hotmail.com>
2022-01-04 13:35:12 +0100

    Added command meals

--- ticket/command.py
+++ ticket/command.py
@@ -1,6 +1,7 @@
 from jinja2 import Environment, FileSystemLoader
 from settings import TEMPLATE_DIR, TICKET_COMMAND_TEMPLATE_NAME, TICKET_COMMAND_LOGO
 import base64
+from models import Command, CommandMeal, Table, Meal
 
 
 def get_edomae_logo() -> str:
@@ -14,24 +15,25 @@ def generate_ticket(id: int) -> str:
     file_loader = FileSystemLoader(TEMPLATE_DIR)
     env = Environment(loader=file_loader)
     template_content = env.get_template(TICKET_COMMAND_TEMPLATE_NAME)
+    
+    command = Command.query.get(id)
+    command_meals = CommandMeal.query.filter_by(command=id)
+    table = Table.query.get(command.table)
 
     data = {
         'EDOMAE_LOGO_BASE64': get_edomae_logo(),
-        'COMMAND_ID': 46,
-        'TABLE_NUMBER': 2,
+        'COMMAND_ID': command.id,
+        'TABLE_NUMBER': table.number,
         'COMMAND_MEALS': [
             {
-                'number': 23,
-                'name': 'Gyozas de pollo',
-                'price': 6.52,
-                'total_price': 6.52
-            },
-            {
-                'number': 23,
-                'name': 'Gyozas de langostino',
-                'price': 6.59,
-                'total_price': 6.59
+                'quantity': command_meal.number,
+                'name': Meal.query.get(command_meal.meal).name,
+                'extra': command_meal.extra,
+                'price': command_meal.price,
+                'total_price': command_meal.total_price,
+                'discount': command_meal.discount
             }
+            for command_meal in command_meals
         ],
         'COMMAND_TOTAL_PRICE': 13,
         'COMMAND_TOTAL_PRICE_WITHOUT_IVA': 10.27,
