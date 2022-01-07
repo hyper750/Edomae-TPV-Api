a4384ae1e69a851af1b7633383408f286caaf783
hyper750 <raulmarquespalmer_150@hotmail.com>
2022-01-07 23:51:05 +0100

    Increment or decrement meal categories order between

--- resources/meal_category.py
+++ resources/meal_category.py
@@ -15,7 +15,7 @@ class MealCategoryResource(Resource):
         category = MealCategory.query.get(id)
 
         if not category:
-            return '', 404
+            return "", 404
 
         return MealCategorySchema().dump(category)
 
@@ -23,15 +23,34 @@ class MealCategoryResource(Resource):
         category = MealCategory.query.get(id)
         if category:
             category.delete()
-        return '', 204
+        return "", 204
 
     def put(self, id: int):
         category = MealCategory.query.get(id)
 
         if not category:
-            return '', 404
+            return "", 404
 
         params = parse_query_meal_category()
+
+        # Increment/Decrement order
+        if to_order := params.get("order"):
+            current_order = category.order
+            if current_order < to_order:
+                categories_to_shift = MealCategory.query.filter(
+                    MealCategory.order >= current_order + 1
+                ).filter(MealCategory.order <= to_order)
+
+                for category_to_shift in categories_to_shift:
+                    category_to_shift.order -= 1
+            else:
+                categories_to_shift = MealCategory.query.filter(
+                    MealCategory.order >= to_order
+                ).filter(MealCategory.order <= current_order - 1)
+
+                for category_to_shift in categories_to_shift:
+                    category_to_shift.order += 1
+
         for key, value in params.items():
             setattr(category, key, value)
 
@@ -44,9 +63,7 @@ class MealCategoriesResource(Resource):
     method_decorators = (jwt_required(),)
 
     def post(self):
-        category = MealCategory(
-            **parse_object_meal_category()
-        )
+        category = MealCategory(**parse_object_meal_category())
 
         # If order is not set, get the it from the last value
         last_order = DB.session.query(func.max(MealCategory.order)).scalar()
