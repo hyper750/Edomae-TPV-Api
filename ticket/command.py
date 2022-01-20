b5a98992028332504251ec0b97fd361a164d4e31
hyper750 <raulmarquespalmer_150@hotmail.com>
2022-01-20 09:22:56 +0100

    Show articles by category

--- ticket/command.py
+++ ticket/command.py
@@ -3,13 +3,15 @@ from typing import List
 
 import arrow
 from jinja2 import Environment, FileSystemLoader
-from models import Command, CommandMeal, Meal, Table, User
+from models import Command, CommandMeal, Meal, Table, User, MealCategory
 from settings import (
     IVA, TEMPLATE_DIR, TICKET_COMMAND_LOGO,
     TICKET_COMMAND_TEMPLATE_NAME, TICKET_SERIE_TEMPLATE_NAME,
     TIMEZONE
 )
 from utils.math import calculate_percentage
+from sqlalchemy import func
+from db.sqlalchemy.sqlalchemy import DB
 
 
 def get_edomae_logo() -> str:
@@ -79,12 +81,82 @@ def generate_ticket(id: int) -> str:
     return template_content.render(**data)
 
 
-def generate_serie_ticket(serie_date: arrow.Arrow, total_price: float) -> str:
+def generate_serie_ticket(serie_date: arrow.Arrow, command_ids: List[int]) -> str:
+    # Total
+    total_price = DB.session.query(func.sum(CommandMeal.total_price)).filter(
+        CommandMeal.command.in_(command_ids)
+    ).scalar()
+
     iva_price = round(
         calculate_percentage(total_price, IVA),
         2
     )
     price_without_iva = round(total_price - iva_price, 2)
+
+    # Meals ordered by category order
+    res = DB.session.query(
+        CommandMeal,
+        Meal,
+        MealCategory
+    ).filter(
+        CommandMeal.meal == Meal.id
+    ).filter(
+        Meal.category == MealCategory.id
+    ).filter(
+        CommandMeal.command.in_(command_ids)
+    ).order_by(
+        MealCategory.order
+    ).all()
+
+    meal_by_categories = []
+    meals = dict()
+    last_category = None
+    total_results = len(res)
+    for i, (command_meal, meal, meal_category) in enumerate(res):
+        # Fill first category
+        if last_category is None:
+            last_category = meal_category.id
+
+        # When changing meals save the last categories
+        # Or is the last item
+        if meal_category.id != last_category:
+            last_category = meal_category.id
+            # Push previos categories data
+            meal_by_categories.append({
+                'name': meal_category.name,
+                'total_price': 0,
+                'meals': list(meals.values()),
+            })
+            meals = dict()
+
+        # Append new meal on that category
+        if meal.id not in meals:
+            meals[meal.id] = dict(
+                name=meal.name,
+                quantity=command_meal.number,
+                price=command_meal.price,
+                total_price=command_meal.total_price
+            )
+        else:
+            # Update price and quantity
+            meals[meal.id]['quantity'] += command_meal.number
+            meals[meal.id]['total_price'] += command_meal.total_price
+
+        # Is last item
+        if i + 1 >= total_results:
+            meal_by_categories.append({
+                'name': meal_category.name,
+                'total_price': 0,
+                'meals': list(meals.values()),
+            })
+
+    # Calculate total price for each category
+    for category in meal_by_categories:
+        total_category_price = sum(
+            meal['total_price']
+            for meal in category.get('meals', [])
+        )
+        category['total_price'] = total_category_price
 
     data = {
         'EDOMAE_LOGO': get_edomae_logo(),
@@ -92,7 +164,8 @@ def generate_serie_ticket(serie_date: arrow.Arrow, total_price: float) -> str:
         'TOTAL_PRICE': round(total_price, 2),
         'TOTAL_PRICE_WITHOUT_IVA': price_without_iva,
         'IVA': IVA,
-        'IVA_PRICE': iva_price
+        'IVA_PRICE': iva_price,
+        'MEAL_CATEGORIES': meal_by_categories
     }
 
     file_loader = FileSystemLoader(TEMPLATE_DIR)
