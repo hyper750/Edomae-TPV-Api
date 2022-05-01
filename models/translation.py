b0228e822421adfba1792eeacdb236de7459b785
hyper750 <raulmarquespalmer_150@hotmail.com>
2022-05-01 22:02:57 +0200

    Added translation model

---
+++ models/translation.py
@@ -0,0 +1,34 @@
+from db.sqlalchemy.sqlalchemy import DB
+from sqlalchemy import Column, BigInteger, String, Enum, UniqueConstraint
+from models.crud_model import CRUDModel
+from language import Language
+
+
+class Translation(DB.Model, CRUDModel):
+
+    ___tablename__ = 'translation'
+
+    __table_args__ = (
+        UniqueConstraint('key', 'language', name='key_language_uc'),
+    )
+
+    # Translation id
+    id = Column(BigInteger, primary_key=True)
+
+    # Given a key to translate
+    key = Column(
+        String(length=2048),
+        nullable=False
+    )
+
+    # And given a language, translate the content to that language
+    language = Column(
+        Enum(Language),
+        nullable=False
+    )
+
+    # We have a translation
+    value = Column(
+        String(length=2048),
+        nullable=False
+    )
