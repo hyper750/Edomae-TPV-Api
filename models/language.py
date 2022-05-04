b74395d4f33984d33e658228ee04986440dce340
hyper750 <raulmarquespalmer_150@hotmail.com>
2022-05-04 18:48:31 +0200

    Value es CA, ES, EN not an id

--- models/language.py
+++ models/language.py
@@ -2,4 +2,7 @@ import enum
 
 from settings import AVAILABLE_LANGUAGES
 
-Language = enum.Enum('Language', AVAILABLE_LANGUAGES)
+Language = enum.Enum('Language', [
+    [lang, lang]
+    for lang in AVAILABLE_LANGUAGES
+])
