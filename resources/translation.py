924e67122402ecaa16edaadbcc4aeaa4921f43da
hyper750 <raulmarquespalmer_150@hotmail.com>
2022-05-04 19:08:40 +0200

    Added translation resource

--- resources/translation.py
+++ resources/translation.py
@@ -1,25 +1,54 @@
-from flask_restful import Resource
+from parser import parse_translation_object, parse_translation_query
+
 from flask_jwt_extended import jwt_required
+from flask_restful import Resource
+from models import Translation
+from serialization import TranslationSchema
 
 
 class TranslationResource(Resource):
     method_decorators = (jwt_required(),)
 
     def get(self, id: int):
-        pass
-    
+        translation = Translation.query.get(id)
+
+        if not translation:
+            return '', 404
+
+        return TranslationSchema().dump(translation)
+
     def put(self, id: int):
-        pass
+        translation = Translation.query.get(id)
+
+        if not translation:
+            return '', 404
+
+        for key, value in parse_translation_query().items():
+            setattr(translation, key, value)
+
+        translation.save()
+
+        return TranslationSchema().dump(translation)
 
     def delete(self, id: int):
-        pass
+        translation = Translation.query.get(id)
+        if translation:
+            translation.delete()
+        return '', 204
 
 
 class TranslationsResource(Resource):
     method_decorators = (jwt_required(),)
 
     def get(self):
-        pass
+        translations = Translation.query.filter_by(
+            **parse_translation_query()
+        ).order_by(Translation.key, Translation.language)
+        return TranslationSchema(many=True).dump(translations)
 
     def post(self):
-        pass
+        translation = Translation(
+            **parse_translation_object()
+        )
+        translation.save()
+        return TranslationSchema().dump(translation), 201
