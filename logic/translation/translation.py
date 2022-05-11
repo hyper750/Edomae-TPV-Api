c725d5b6770a74ca808c461368d712bcead4daf1
hyper750 <raulmarquespalmer_150@hotmail.com>
2022-05-11 18:51:23 +0200

    Save translation outside the operator

--- logic/translation/translation.py
+++ logic/translation/translation.py
@@ -2,7 +2,11 @@ from models import Language, Translation
 
 
 def get_translation(key: str, language: Language) -> str:
-    if translation := Translation.query.filter_by(key=key, language=language).first() is not None:
+    translation = Translation.query.filter_by(
+        key=key, language=language
+    ).first()
+
+    if translation is not None:
         return translation.value
 
     # If the translation is not found, return the key
